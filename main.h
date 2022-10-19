b9993449fbdb2bb75dc512af07e241214a8c9a7d
onome <onomemafuru@gmail.com>
2022-10-19 16:13:40 -0700

    g

--- main.h
+++ main.h
@@ -1,5 +1,3 @@
-
-
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
@@ -8,13 +6,13 @@
 #include <stdlib.h>
 
 /**
- *  * struct print - struct for printer functions
- *   * @type_arg: identifier
- *    * @f: pointer to a printer functions
- *     *
- *      * Description: struct that stores pointers to a
- *       * printer functions.
- *        */
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
 typedef struct print
 {
 		char *type_arg;
